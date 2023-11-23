72dbe18a9219630c63c453c2f15c568574a783d7
Dmytro Oliinyk <oliynykdmitry83@gmail.com>
2023-11-23 11:30:53 +0000

    some data

--- lect8.py
+++ lect8.py
@@ -1,1 +1,8 @@
-import numpy as np
+import numpy as np # importing NumPy 
+
+#  Guidance:
+#  Comments made by hash symbol are not interactive and moustly contain some explanation or theory
+#  Other coments by set of triple double quotes """some code""" are interactive part of code you can play with
+#  Be aware that they Always! go in paires so to comment/uncomment code you need to keep that in your mind.
+
+ 
