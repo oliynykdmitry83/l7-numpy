b39cf4d4f8f66eb70054b3661049355283b79150
Dmytro Oliinyk <oliynykdmitry83@gmail.com>
2023-11-28 14:39:22 +0000

    some formating added

--- lect8.py
+++ lect8.py
@@ -8,79 +8,14 @@
 
 #  To "install" NumPy go to "Tools" -> "Packages" -> Numpy -> install
 
-# import numpy as np # importing NumPy
 
 
 
-# The following code will contain a range of exercises using NumPy, 
-# it is recommended that you start at the top and work your way down in stages to help support your development 
+##################  NumPy Methods  #####################
 
 
-# Task 01 - Create an array
-# using NumPy, create an array wich contains the below numbers, you should print this out the screen in ordre to test
-# [1 4 6 8 7 2 3 9 10 13 5]
-
-# Displaying odd numbers
-# Using the array you've created, display only odd numbers that are within the array e.g.
-# [1 7 3 9 13 5]
-
-# creating a Matrix
-# Create an identity matrix with the dimentions of 4*4. The below shows the example output of this:
-# [[1 0 0 0
-#   0 1 0 0
-#   0 0 1 0
-#   0 0 0 1]]
-
-# Mathmatical Arrays
-# Within the following challenges (all labelled Math #value), use the below code at the start:
-
-"""
-# Imports NumPy in order to be used
-import numpy as np
-
-# Creates an array of data
-randomArray = np.array([[1005, 2154, 1123,6524],
-                        [1136, 8874, 4665, 4785],
-                        [3365, 9564, 4474, 5632],
-                        [1111, 3354, 6625, 2247],
-                        [1679, 4652, 6431, 1465]])
-""" 
-
-# Math #1
-# Display the avarage of the above array
-
-# Math #2
-# Start by displaying the numbers within the 3rd column with this array.
-# Using these values, provide the avarage value of the third column within this array
-# Display this value to 2 decimal places
-
-# Math #3
-# Provide the avarage value of the first 2 rows withing this array. 
-
-# Math #4
-# Provide the minimum and maximun value withing this array
-
-
-##############################################################################
-
-# NumPy Challenge Scenario:
+# ############################################################
 #
-# Student Grade Tracker
-# 
-# 15 students have been provided with grades the max mar avaliable is 100.
-# Allow for the user to put these values into a NumPy array.
-# 
-# Once this has been completed, you will need to comlete the following using this dataset:
-#   • Work out the minimum, maximun and avarange grade.
-#   • Sort the grades in ascending order
-#   • All points will be provided with 2 extra marks, display the output for this.
-#   * Display the tpo three  and lowest three grades scored.
-# Also if you can save and retrieve this data back from a file after you have used it.
-
-###############################################################################
-
-# NumPy Methods
-# 
 # Sorting Values
 # 
 # We can use NumPy to organise data within our array/matrix,  these use the .sort() method.
@@ -219,4 +154,75 @@ print(emptyArray)
 
 # Typically,  using np.empty() will be quicker for a large matrices, due to machine not needing to
 # initialise every element with a specific value. 
+
+
+# The following code will contain a range of exercises using NumPy, 
+# it is recommended that you start at the top and work your way down in stages to help support your development 
+
+
+# Task 01 - Create an array
+# using NumPy, create an array wich contains the below numbers, you should print this out the screen in ordre to test
+# [1 4 6 8 7 2 3 9 10 13 5]
+
+# Displaying odd numbers
+# Using the array you've created, display only odd numbers that are within the array e.g.
+# [1 7 3 9 13 5]
+
+# creating a Matrix
+# Create an identity matrix with the dimentions of 4*4. The below shows the example output of this:
+# [[1 0 0 0
+#   0 1 0 0
+#   0 0 1 0
+#   0 0 0 1]]
+
+# Mathmatical Arrays
+# Within the following challenges (all labelled Math #value), use the below code at the start:
+
+"""
+# Imports NumPy in order to be used
+import numpy as np
+
+# Creates an array of data
+randomArray = np.array([[1005, 2154, 1123,6524],
+                        [1136, 8874, 4665, 4785],
+                        [3365, 9564, 4474, 5632],
+                        [1111, 3354, 6625, 2247],
+                        [1679, 4652, 6431, 1465]])
+""" 
+
+
+############################################################################
+
+# Math #1
+# Display the avarage of the above array
+
+# Math #2
+# Start by displaying the numbers within the 3rd column with this array.
+# Using these values, provide the avarage value of the third column within this array
+# Display this value to 2 decimal places
+
+# Math #3
+# Provide the avarage value of the first 2 rows withing this array. 
+
+# Math #4
+# Provide the minimum and maximun value withing this array
+
+
+##############################################################################
+
+# NumPy Challenge Scenario:
+#
+# Student Grade Tracker
+# 
+# 15 students have been provided with grades the max mar avaliable is 100.
+# Allow for the user to put these values into a NumPy array.
+# 
+# Once this has been completed, you will need to comlete the following using this dataset:
+#   • Work out the minimum, maximun and avarange grade.
+#   • Sort the grades in ascending order
+#   • All points will be provided with 2 extra marks, display the output for this.
+#   * Display the tpo three  and lowest three grades scored.
+# Also if you can save and retrieve this data back from a file after you have used it.
+
+###############################################################################
 
