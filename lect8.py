ad5c4b076c8e7171a8c7571683eccde450e84bfb
Dmytro Oliinyk <oliynykdmitry83@gmail.com>
2023-11-28 10:08:17 +0000

    Math arrays

--- lect8.py
+++ lect8.py
@@ -1,8 +1,45 @@
-import numpy as np # importing NumPy 
-
 #  Guidance:
 #  Comments made by hash symbol are not interactive and moustly contain some explanation or theory
 #  Other coments by set of triple double quotes """some code""" are interactive part of code you can play with
 #  Be aware that they Always! go in paires so to comment/uncomment code you need to keep that in your mind.
 
- 
+
+#  To "install" NumPy go to "Tools" -> "Packages" -> Numpy -> install
+
+# import numpy as np # importing NumPy
+
+
+
+# The following code will contain a range of exercises using NumPy, 
+# it is recommended that you start at the top and work your way down in stages to help support your development 
+
+
+# Task 01 - Create an array
+# using NumPy, create an array wich contains the below numbers, you should print this out the screen in ordre to test
+# [1 4 6 8 7 2 3 9 10 13 5]
+
+# Displaying odd numbers
+# Using the array you've created, display only odd numbers that are within the array e.g.
+# [1 7 3 9 13 5]
+
+# creating a Matrix
+# Create an identity matrix with the dimentions of 4*4. The below shows the example output of this:
+# [[1 0 0 0
+#   0 1 0 0
+#   0 0 1 0
+#   0 0 0 1]]
+
+# Mathmatical Arrays
+# Within the following challenges (all labelled Math #value), use the below code at the start:
+
+#"""
+# Imports NumPy in order to be used
+import numpy as np
+
+# Creates an array of data
+randomArray = np.array([[1005, 2154, 1123,6524],
+                        [1136, 8874, 4665, 4785],
+                        [3365, 9564, 4474, 5632],
+                        [1111, 3354, 6625, 2247],
+                        [1679, 4652, 6431, 1465]])
+#""" 
